5ce0308af797fe7de5881063cae0d6d2c8e1143d
OneOverCosine <TheCandyDragon@gmail.com>
2021-03-29 16:52:17 +0100

    Updated orders files

--- orders.py
+++ orders.py
@@ -6,7 +6,7 @@ def open_and_read_file(filename):
     
     try:
         # built in method for opening files - open("filename")
-        file = open(filename)
+        file = open(filename, "r+")
         print(f"{filename} opened!")
     except FileNotFoundError as err:
         print(err) # prints the error message to the console
@@ -14,16 +14,37 @@ def open_and_read_file(filename):
     else:
         # this only runs if the try didn't raise any exceptions
         # put data from file into a list
-        orders = file.readlines()
+        orders = []
             
-        for i in range(len(orders)):
-            orders[i] = orders[i].replace("\n", "")
+        for line in file.readlines():
+            orders.append(line.strip("\n"))
 
         print(orders)
+
+        file.write(write_to_file())
 
     finally:
         # always executes, regardless of errors
         print("I always run!")
+        return file
 
+def write_to_file():
 
-open_and_read_file(filename)
+    user_input = input("Add an item to the list\n")
+
+    while True:
+        confirm_input = input(f"Are you sure you want to add {user_input}\n")
+
+        if confirm_input.lower() == 'yes':
+            return "\n" + user_input
+        else:
+            user_input = input("Add an item to the list\n")
+
+orders_file = open_and_read_file(filename)
+orders_file.seek(0)
+
+# wip
+for line in orders_file.readlines():
+    print(line.strip("\n"))
+
+orders_file.close()
